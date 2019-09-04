5602e6e36e1bef87197bc99c54dc8916177994a2
(w) Iker Hernaez <i.hernaez@hif-soft.net>
2019-09-04 17:13:23 +0200

    Added package-info

---
+++ src/main/java/dev/orne/config/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>Configuration management system. Provides an abstract method to retrieve
+ * configuration parameters through the library and applications in a
+ * underlying configuration storage method agnostic way.</p>
+ * 
+ * @since 1.0, 2019-07
+ */
+package dev.orne.config;
